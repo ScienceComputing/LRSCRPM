64404611c256edae75bbe788abf8790d158afcd5
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-28 16:10:15 -0400

    Update LRSCRPM.py

--- LRSCRPM.py
+++ LRSCRPM.py
@@ -5,7 +5,3 @@ LRSCRPM - Integrative workflow for long read single cell RNA-seq data processing
 __version__ = "1.0.0"
 
 
-pip3 install Biopython pandas numpy tqdm matplotlib levenshtein
-
-python3 blaze.py --expect-cells=1000 --threads=20 path/to/fastq_pass
-
